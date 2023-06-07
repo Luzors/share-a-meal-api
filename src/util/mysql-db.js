f0820f049e44c40ef6bd1318b25916362e047dfa
Luzors <jornvanbommel@gmail.com>
2023-06-07 16:53:50 +0200

    Initial commit

---
+++ src/util/mysql-db.js
@@ -0,0 +1,18 @@
+// get the client
+const mysql = require('mysql2');
+
+// Create the connection pool. The pool-specific settings are the defaults
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  database: process.env.NODE_ENV==="test" ? "shareamealtest" : undefined  || process.env.DB_DATABASE || 'shareameal',
+  port: process.env.DB_PORT || 3306,
+  password: process.env.DB_PASSWORD || '',
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0
+});
+
+module.exports = pool;
